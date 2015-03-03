e58e5fb80e0e8683e0f43ffd5108dccb79ad38d8
Jestin <jestintab@gmail.com>
2015-03-03 23:16:26 +0530

    Read me updated list view xml created java coding in progress

--- app/src/main/java/com/rj/tapnote/MainActivity.java
+++ app/src/main/java/com/rj/tapnote/MainActivity.java
@@ -4,49 +4,71 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
 
-    public ListView list;
 
-    public String[] noteHeadings;
+    String[] noteHeadings;
     String[] noteExcerpts;
     String[] noteTags;
-    Resources res = getResources();
 
+    ListView list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public String[] getRes() {
+        Resources res = getResources();
+
 
         noteHeadings = res.getStringArray(R.array.note_head);
         noteExcerpts = res.getStringArray(R.array.note_excerpt);
         noteTags = res.getStringArray(R.array.note_tag);
 
-        return noteHeadings;
 
-    }
-
-
-    @Override
-    public View findViewById(int id) {
         list = (ListView) findViewById(R.id.main_list);
-        return list;
+
+
+        TapNote a = new TapNote(this, noteHeadings, noteExcerpts, noteTags);
+        list.setAdapter(a);
     }
+
 
 }
 
 class TapNote extends ArrayAdapter<String> {
 
-    TapNote(Context c, String[] noteHeadings) {
-        super(c, R.layout.single_row, R.id.note_head, noteHeadings);
+    Context context;
+    String[] headArray;
+    String[] excerptArray;
+    String[] tagArray;
 
+    TapNote(Context c, String[] noteHeadings, String[] noteExcerpts, String[] noteTags) {
+        super(c, R.layout.single_row1, R.id.note_head, noteHeadings);
+        this.headArray = noteHeadings;
+        this.excerptArray = noteExcerpts;
+        this.headArray = noteTags;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.single_row1, parent, false);
+        TextView tnHead = (TextView) row.findViewById(R.id.note_head);
+        TextView tnExcerpt = (TextView) row.findViewById(R.id.note_excerpt);
+        TextView tnTag = (TextView) row.findViewById(R.id.note_tag);
+
+        tnHead.setText(headArray[position]);
+        tnExcerpt.setText(excerptArray[position]);
+        tnTag.setText(tagArray[position]);
+
+        return row;
     }
 }
