81c6314b46f62e33bbd35ae4a322783f13083ba1
Robert Mathew <robu1691@gmail.com>
2015-03-04 10:40:26 +0530

    * added ToolBar * added some styles

--- app/src/main/java/com/rj/tapnote/MainActivity.java
+++ app/src/main/java/com/rj/tapnote/MainActivity.java
@@ -1,47 +1,69 @@
 package com.rj.tapnote;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+public class MainActivity extends ActionBarActivity {
 
-public class MainActivity extends Activity {
-
-
-    String[] noteHeadings;
-    String[] noteExcerpts;
-    String[] noteTags;
+    private Toolbar toolbar;
+    String[] noteHeadings, noteExcerpts, noteTags;
 
     ListView list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
         Resources res = getResources();
-
-
         noteHeadings = res.getStringArray(R.array.note_head);
         noteExcerpts = res.getStringArray(R.array.note_excerpt);
         noteTags = res.getStringArray(R.array.note_tag);
 
-
         list = (ListView) findViewById(R.id.main_list);
-
-
         TapNote a = new TapNote(this, noteHeadings, noteExcerpts, noteTags);
         list.setAdapter(a);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast.makeText(this, "Just clicked Setting", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
 
-class TapNote extends ArrayAdapter<String>{
+class TapNote extends ArrayAdapter<String> {
 
     Context context;
     String[] headArray;
@@ -63,7 +85,6 @@ class TapNote extends ArrayAdapter<String>{
         TextView tnHead = (TextView) row.findViewById(R.id.note_head);
         TextView tnExcerpt = (TextView) row.findViewById(R.id.note_excerpt);
         TextView tnTag = (TextView) row.findViewById(R.id.note_tag);
-
         tnHead.setText(headArray[position]);
         tnExcerpt.setText(excerptArray[position]);
         tnTag.setText(tagArray[position]);
