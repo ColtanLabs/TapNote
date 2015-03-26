9726db4ecad2209133fb165a03ca0e572ac3453f
Robert Mathew <robu1691@gmail.com>
2015-03-26 18:10:13 +0530

    home button for settings page

--- app/src/main/java/com/rj/tapnote/SettingsActivity.java
+++ app/src/main/java/com/rj/tapnote/SettingsActivity.java
@@ -1,8 +1,10 @@
 package com.rj.tapnote;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class SettingsActivity extends ActionBarActivity {
 
@@ -13,10 +15,24 @@ public class SettingsActivity extends ActionBarActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Display the fragment as the main content.
         getFragmentManager().beginTransaction()
                 .replace(R.id.frame_container, new SettingsFragment())
                 .commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
