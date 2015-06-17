d3c39fbb5e2bf32a4eb4565bd42a8d8c45c6d22c
Robert Mathew <robu1691@gmail.com>
2015-06-17 13:17:45 +0530

    Added search funutionality

--- app/src/main/java/com/coltan/tapnote/NoteAdapter.java
+++ app/src/main/java/com/coltan/tapnote/NoteAdapter.java
@@ -4,13 +4,17 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> implements Filterable {
 
     private ArrayList<String> mDataset, mSubHeader, mTag;
+    private ArrayList<String> appListSearch;
+    private int mWhichFrag;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
@@ -41,10 +45,11 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public NoteAdapter(ArrayList<String> myHeader, ArrayList<String> mySubHeader, ArrayList<String> myTag) {
+    public NoteAdapter(ArrayList<String> myHeader, ArrayList<String> mySubHeader, ArrayList<String> myTag, int whichFrag) {
         mDataset = myHeader;
         mSubHeader = mySubHeader;
         mTag = myTag;
+        mWhichFrag = whichFrag;
     }
 
     // Create new views (invoked by the layout manager)
@@ -64,13 +69,6 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
         // - replace the contents of the view with that element
         //final String name = mDataset.get(position);
         holder.txtHeader.setText(mDataset.get(position));
-        /*holder.txtHeader.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //remove(name);
-            }
-        });*/
-
         holder.txtFooter.setText(mSubHeader.get(position));
         if (mTag.get(position).equals("")) {
             holder.txtTag.setVisibility(View.INVISIBLE);
@@ -86,4 +84,51 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
         return mDataset.size();
     }
 
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                final FilterResults oReturn = new FilterResults();
+                final ArrayList<String> results = new ArrayList<>();
+                if (appListSearch == null) {
+                    appListSearch = mDataset;
+                }
+                if (charSequence != null) {
+                    if (appListSearch != null && appListSearch.size() > 0) {
+                        for (final String rNote : appListSearch) {
+                            if (rNote.toLowerCase().contains(charSequence.toString())) {
+                                results.add(rNote);
+                            }
+                        }
+                    }
+                    oReturn.values = results;
+                    oReturn.count = results.size();
+                }
+                return oReturn;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                if (mWhichFrag == 0) {
+                    if (filterResults.count > 0) {
+                        HomeFragment.setResultsMessage(false);
+                    } else {
+                        HomeFragment.setResultsMessage(true);
+                    }
+
+                } else {
+                    if (filterResults.count > 0) {
+                        StarredFragment.setResultsMessage(false);
+                    } else {
+                        StarredFragment.setResultsMessage(true);
+                    }
+                }
+                mDataset = (ArrayList<String>) filterResults.values;
+                notifyDataSetChanged();
+            }
+
+
+        };
+
+    }
 }
--- app/src/main/java/com/coltan/tapnote/StarredFragment.java
+++ app/src/main/java/com/coltan/tapnote/StarredFragment.java
@@ -9,11 +9,18 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.coltan.tapnote.db.DatabaseHandler;
 import com.coltan.tapnote.db.Note;
@@ -21,13 +28,18 @@ import com.coltan.tapnote.db.Note;
 import java.util.ArrayList;
 import java.util.List;
 
-public class StarredFragment extends Fragment implements RecyclerItemClickListener.OnItemClickListener {
+public class StarredFragment extends Fragment implements RecyclerItemClickListener.OnItemClickListener, SearchView.OnQueryTextListener {
 
     private ArrayList<String> mId = new ArrayList<>();
     private ArrayList<String> mHeader = new ArrayList<>();
     private ArrayList<String> mSubHeader = new ArrayList<>();
     private ArrayList<String> mTag = new ArrayList<>();
     private ArrayList<String> mNote = new ArrayList<>();
+
+    private RecyclerView mRecyclerView;
+    private MenuItem searchItem;
+    private SearchView searchView;
+    private static LinearLayout noResults;
 
     public StarredFragment() {
         // Required empty public constructor
@@ -36,9 +48,11 @@ public class StarredFragment extends Fragment implements RecyclerItemClickListen
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_starred, container, false);
-        RecyclerView mRecyclerView = (RecyclerView) v.findViewById(R.id.my_recycler_view);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.my_recycler_view);
+        noResults = (LinearLayout) v.findViewById(R.id.noResults);
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
@@ -50,7 +64,7 @@ public class StarredFragment extends Fragment implements RecyclerItemClickListen
         mRecyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), null));
         initData();
 
-        RecyclerView.Adapter mAdapter = new NoteAdapter(mHeader, mSubHeader, mTag);
+        RecyclerView.Adapter mAdapter = new NoteAdapter(mHeader, mSubHeader, mTag, 1);
         mRecyclerView.setAdapter(mAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.fab);
@@ -79,11 +93,8 @@ public class StarredFragment extends Fragment implements RecyclerItemClickListen
     @Override
     public void onItemLongPress(View childView, int position) {
         // Do another thing when an item is long pressed.
-        // Gets a handle to the clipboard service.
         ClipboardManager clipboard = (ClipboardManager) getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
-        // Creates a new text clip to put on the clipboard
         ClipData clip = ClipData.newPlainText(mHeader.get(position), mNote.get(position));
-        // Set the clipboard's primary clip.
         clipboard.setPrimaryClip(clip);
         Snackbar.make(childView, "Copied to clipboard", Snackbar.LENGTH_SHORT).show();
     }
@@ -110,4 +121,59 @@ public class StarredFragment extends Fragment implements RecyclerItemClickListen
     }
 
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_home, menu);
+        searchItem = menu.findItem(R.id.action_search);
+        searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        if (newText.isEmpty()) {
+            ((NoteAdapter) mRecyclerView.getAdapter()).getFilter().filter("");
+        } else {
+            ((NoteAdapter) mRecyclerView.getAdapter()).getFilter().filter(newText);
+        }
+
+        return false;
+    }
+
+    public static void setResultsMessage(Boolean result) {
+        if (result) {
+            noResults.setVisibility(View.VISIBLE);
+        } else {
+            noResults.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        getView().setFocusableInTouchMode(true);
+        getView().requestFocus();
+        getView().setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if (keyEvent.getAction() == KeyEvent.ACTION_UP && i == KeyEvent.KEYCODE_BACK) {
+                    // handle back button's click listener
+                    if (searchItem.isVisible() && !searchView.isIconified()) {
+                        searchView.onActionViewCollapsed();
+                    } else {
+                        getActivity().finish();
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
 }
